048c87854d49cddd5fc355dfdd9c42ec00a0c3fc
kir_rik <kir-rik-90@yandex.ru>
2020-08-14 11:11:10 +0400

    sum tests

--- src/utils/__tests__/sum.test.js
+++ src/utils/__tests__/sum.test.js
@@ -1,7 +1,25 @@
 const sum = require('../sum');
 
 describe("sum", () => {
-  test("adds 1 + 2 to equal 3", () => {
+  test("positive numbers", () => {
     expect(sum(1, 2)).toBe(3);
   });
+
+  test("negative numbers", () => {
+    expect(sum(-1, -2)).toBe(-3);
+  });
+
+  test("add zero", () => {
+    expect(sum(1, 0)).toBe(1);
+  });  
+  
+  test("floats", () => {
+    expect(sum(0.1, 0.2)).toBe(0.3);
+  });
+
+  test("not a number", () => {
+    expect(sum(1, '2')).toBe(NaN);
+    expect(sum('1', 2)).toBe(NaN);
+    expect(sum(1, NaN)).toBe(NaN);
+  })
 });
